eb06c57f1c1942593e53d7a68194df4ce5147aa3
vincentwongmax <vincentwongmax@gmail.com>
2020-06-19 17:03:15 +0800

    0.0015

--- index.php
+++ index.php
@@ -4,92 +4,151 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
         integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+
 </head>
 
 <body>
+    <!-- <div class="d1">
+        <div class="d2">
+            <div class="d3">
+                <label><input type="checkbox" checked><span>焼豚</span></label>
+                
+       <label><input type="checkbox" checked><span>メンマ</span></label>
+                
+      <label><input type="checkbox"><span>ニンニク</span></label>
+                
+       <label><input type="checkbox" checked><span>ねぎ</span></label>
+                
+       <label><input type="checkbox"><span>味玉</span></label>
+            </div>
+        </div>
+        <div class="d2">
+            <div class="d3">
+                <label><input type="radio" name="m3"><span>バリカタ
+                    </span></label><label><input type="radio" name="m3" checked><span>硬め
+                    </span></label><label><input type="radio" name="m3"><span>普通
+                    </span></label><label><input type="radio" name="m3"><span>柔らかめ
+                    </span></label>
+            </div>
+        </div>
+    </div> -->
+
+
     <H1>旅行用記帳平分器 測試版V.0.009</H1>
 
-    <button class="btn btn-outline-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
-    新增入物
-  </button>
 
-    <div id="payMainMoneyPeople">
-        <h2>請輸入或建立行程代號</h2>
+
+    <div id="payMainMoneyPeople" >
+        <h2><mark>1. 請輸入或建立行程代號<mark></h2>
         <input id="ecToken" required="required" type="text" name="token" placeholder="可數英中台語廣東語馬來文" />
         <button type="button" class="btn btn-outline-info" onclick="enterToken();">輸入</button>
         <button type="button" class="btn btn-outline-info" onclick="createToken()">建立</button>
     </div>
     <br>
-<div class="start" id="start"  style="display:none;" >
-    <div class="collapse" id="collapseExample">
-        <h2>請輸入以建立人物名稱</h2>
-        <input id="createName" type="text" name="token" placeholder="請輸入或建立" />
-        <button type="button" class="btn btn-outline-info" onclick="createName()">輸入</button>
-    </div>
-    <h1> 目前人物成員有:
-        <a id="number"> </a>
-        <button type="button" class="btn btn-outline-info" onclick="openNameShow()">打開</button>
-        <h2 id="nameShow" style="display:none;"></h2>
-    </h1>
+    <div class="start" id="start" style="display:none;">
+        
+        <h2><mark>2.目前人物成員有:
+            <a id="number"> </a><mark>
+            <button type="button" class="btn btn-outline-info" onclick="openNameShow()">打開</button>
+            <button class="btn btn-outline-primary" type="button" data-toggle="collapse" data-target="#collapseExample"
+                aria-expanded="false" aria-controls="collapseExample">
+                新增入物
+            </button>
 
-    <br><br>
-
-    <div id="payMainMoneyPeople">
-        <h1>付錢人(單選)</h1>
-        <h3 id="payMainMoneyPeople2"></h3>
-        </h1>
-    </div>
-
-    <br><br>
-    <h2>請輸入付款多少錢</h2>
-    <input id="howmuchmoney" required="required" type="text" placeholder="請輸入付款多少錢"> </input>
-    <br><br>
-
-    <div id="userMoneyPeople">
-        <h1>受益人(可多選)</h1>
-        <h3 id="userMoneyPeople2"></h3>
-        </h1>
-    </div>
-
-    <input type="button" class="btn btn-outline-info" value="提交" onclick="getAll()"><br />
-
-    <br><br><br>
-
-    <table id="showWaterBill" class="table table-hover">
-        <thead>
-            <tr>
-                <th scope="col">ID</th>
-                <th scope="col">paymoneypeople</th>
-                <th scope="col">usermoneypeople</th>
-                <th scope="col">howmuchmoney</th>
-            </tr>
-        </thead>
-        <tbody>
-        </tbody>
-    </table>
+            <h2 id="nameShow" style="display:none;"></h2>
+        </h2>
+        
+        <div class="collapse" id="collapseExample">
+            <h3> 請輸入以建立人物名稱 </h3>
+            <input id="createName" type="text" name="token" placeholder="請輸入或建立" />
+            <button type="button" class="btn btn-outline-info" onclick="createName()">輸入</button>
+        </div>
     <br>
-   
-    <h4 id="showPersonMoney"></h4>
-    <br><br><br>
+        <h2><mark>3.記帳
+        <button class="btn btn-outline-primary" type="button" data-toggle="collapse" data-target="#recordItNow"
+                aria-expanded="false" aria-controls="recordItNow">
+                打開
+        </button></mark></h2>
 
-    
-    <h4 id="paypaypay"></h4>
+
+
+        <div id="recordItNow" class="collapse"  >
+        <div id="payMainMoneyPeople">
+            <h1>付錢人(單選)</h1>
+            <h3 id="payMainMoneyPeople2"></h3>
+            </h1>
+        </div>
+
+        <br><br>
+        <h2>請輸入付款多少錢</h2>
+        <input id="howmuchmoney" required="required" type="text" placeholder="請輸入付款多少錢"> </input>
+        <br><br>
+
+        <div id="userMoneyPeople">
+            <h1>受益人(可多選)</h1>
+            <table id="userMoneyPeople2" class="table table-bordered">
+
+                <tbody>
+
+                </tbody>
+            </table>
+            <h4></h4>
+            </h1>
+        </div>
+        <input type="button" class="btn btn-outline-info" value="提交" onclick="getAll()"><br />
+        </div>
+        <br>
+        <h2><mark>4.顯示明細
+        <button class="btn btn-outline-primary" type="button" data-toggle="collapse" data-target="#showbill"
+                aria-expanded="false" aria-controls="showbill">
+                打開
+        </button></mark></h2>
+
+
+    <div id="showbill" class="collapse" >
+        <table id="showWaterBill" class="table table-hover">
+            <thead>
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">paymoneypeople</th>
+                    <th scope="col">usermoneypeople</th>
+                    <th scope="col">howmuchmoney</th>
+                </tr>
+            </thead>
+            <tbody>
+            </tbody>
+        </table>
+        <br>
+
+        <h4 id="showPersonMoney"></h4>
+        <br><br><br>
+
+
+        <h4 id="paypaypay"></h4>
+
 </div>
+    </div>
     </br>
+
+
+
+
+
+
     <script>
         let getUrlString = location.href;
         let url = new URL(getUrlString);
         let get = url.searchParams.get('token');
 
         if (get == null || get == '') {
-        //     abc();
-        // } else {
-        //     document.getElementById('ecToken').value = get;
-        //     enterToken();
-        }else{
+            //     abc();
+            // } else {
+            //     document.getElementById('ecToken').value = get;
+            //     enterToken();
+        } else {
             document.getElementById('ecToken').value = get;
         }
-       
+
         // function abc() {
         //     var input = prompt("請輸入token");
 
@@ -112,9 +171,11 @@
         var userMoneyPeople;
         var payMainMoneyPeople;
         var createNamee;
-        function openNameShow(){
+
+        function openNameShow() {
             $('#nameShow').toggle();
         }
+
         function getAll() {
             let payMainMoneyPeople = $('input:radio[name="box"]:checked').map(function () {
                 return $(this).val();
@@ -124,12 +185,13 @@
             }).get().join(",");
             let howmuchmoney = $('#howmuchmoney').val();
 
-            if(payMainMoneyPeople == undefined || userMoneyPeople == undefined || howmuchmoney ==undefined || howmuchmoney == 0){
+            if (payMainMoneyPeople == undefined || userMoneyPeople == undefined || howmuchmoney == undefined ||
+                howmuchmoney == 0) {
                 alert("輸入內容不允許為空")
-            }else{
-                if(payMainMoneyPeople == '' || userMoneyPeople == '' || howmuchmoney == ''){
+            } else {
+                if (payMainMoneyPeople == '' || userMoneyPeople == '' || howmuchmoney == '') {
                     alert("輸入內容不允許為空")
-                }else{
+                } else {
                     dataToDB(payMainMoneyPeople, userMoneyPeople, howmuchmoney);
                 }
             }
@@ -145,9 +207,9 @@
                     },
                 })
                 .then(function (response) {
-                    $('.start').toggle();
+                    $('.start').show();
                     show($('#ecToken').val());
-                    if($('#ecToken').val() == null || $('#ecToken').val()== ''){
+                    if ($('#ecToken').val() == null || $('#ecToken').val() == '') {
                         alert('請輸入TOKEN');
                     }
                     //   console.log(response.data[0]);
@@ -222,30 +284,77 @@
                     $('#number').html(`${response.data.length}位`);
                     $('#nameShow').html(item.join(''));
 
-                    for (let i = 0, len = response.data.length; i < len; i++) {
-                        number++;
-                        item2.push(
-                            `
-                                <tr>
-                                    <td>${response.data[i].mainpeople} ： <td>
-                                    <td><input type="radio" name="box" value="${response.data[i].mainpeople}"/><td>
-                                </tr>
-                            `
-                        );
-                    }
-                    $('#payMainMoneyPeople2').html(item2.join(''));
+
+                    item2.push(
+                        `    
+                        <div class="d1">                        
+                        <div class="d2">
+                        <div class="d3">
+                         `
+                    )
 
                     for (let i = 0, len = response.data.length; i < len; i++) {
                         number++;
-                        item3.push(
-                            `
-                                <tr>
-                                    <td>${response.data[i].mainpeople} ： <td>
-                                    <td><input type="checkbox" name="boxs" value="${response.data[i].mainpeople}"/><td>
-                                </tr>
+
+                        if (i % 3 == 0) {
+                            item2.push(
                                 `
+                                <label><input type="radio" name="box" value="${response.data[i].mainpeople}" ><span> ${response.data[i].mainpeople} </span></label></br>
+                                `
+                            );
+                        } else {
+                            item2.push(
+                                `
+                                <label><input type="radio" name="box" value="${response.data[i].mainpeople}" ><span> ${response.data[i].mainpeople} </span></label>
+                                `
+                            );
+                        }
+                    }
+
+                    item2.push(
+                        `   
+                            </div>
+                            </div>
+                            </div>
+
+                        `
+                    )
+
+                    $('#payMainMoneyPeople2').html(item2.join(''));
+
+                    item3.push(
+                        `    
+                            <div class="d1">
+                            <div class="d2">
+                            <div class="d3">
+                        `
+                    )
+
+
+                    for (let i = 0, len = response.data.length; i < len; i++) {
+                        number++;
+                        if (i % 3 == 0) {
+                            item3.push(
+                                `
+                             <label><input type="checkbox" name="boxs" value="${response.data[i].mainpeople}" ><span> ${response.data[i].mainpeople} </span></label><br>
+                            `
+                            );
+                        } else(
+                            item3.push(
+                                `
+                              <label><input type="checkbox" name="boxs" value="${response.data[i].mainpeople}" ><span> ${response.data[i].mainpeople} </span></label>
+                            `
+                            )
                         );
                     }
+
+                    item2.push(
+                        `      
+                            </div>
+                            </div>
+                            </div>
+                        `
+                    )
                     $('#userMoneyPeople2').html(item3.join(''));
                 })
                 .catch(function (error) {
@@ -262,7 +371,7 @@
                     },
                 })
                 .then(function (response) {
-                    
+
                     createName2 = $('#createName').val();
                     $('#nameShow').append(`<tr><td>${createName2}</td></tr>`);
                     alert('DONE');
@@ -326,7 +435,7 @@
                     console.log(error);
                 });
         }
-     
+
         function showWaterBillAccount(msg) {
             for (let i = 0, len = msg.length; i < len; i++) {
 
@@ -349,16 +458,16 @@
                 }
             }
 
-           abc =families;
-           $('#showPersonMoney').empty();
-           $('#showPersonMoney').append(`<h3 style="color:cadetblue"> 目前每個人的錢 </h3>`)
-           families.forEach(element => {
-           $('#showPersonMoney').append(`${element.name} =>  ${element.money} <br>`)
-         });
+            abc = families;
+            $('#showPersonMoney').empty();
+            $('#showPersonMoney').append(`<h3 style="color:cadetblue"> 目前每個人的錢 </h3>`)
+            families.forEach(element => {
+                $('#showPersonMoney').append(`${element.name} =>  ${element.money} <br>`)
+            });
 
             families.forEach(element => {
-            element.money = element.money * -1;
-         });
+                element.money = element.money * -1;
+            });
 
             sortByKey(families, 'money'); //json, 排序用的key
             $('#paypaypay').empty();
@@ -375,24 +484,24 @@
         }
 
         function wtfwhocare(wtf) {
-        let people=wtf.length-1;
+            let people = wtf.length - 1;
 
             for (let i = 0; i <= people; i++) //print 人數 
-            {   
+            {
                 if (wtf[people].money > wtf[i].money) {
-                    let  x, y, z;
+                    let x, y, z;
                     x = wtf[i].money;
                     y = wtf[people].money;
                     z = x + y;
                     wtf[i].money = wtf[people].money + wtf[i].money;
-                    
-                    if(z > 0) {
-                        if(x*-1 != 0){
+
+                    if (z > 0) {
+                        if (x * -1 != 0) {
                             $('#paypaypay').append(`${wtf[people].name} ==>${wtf[i].name}`);
                         }
-                        
-                    }else{
-                        if(y != 0){
+
+                    } else {
+                        if (y != 0) {
                             $('#paypaypay').append(`${wtf[people].name} ==>${wtf[i].name}`);
                         }
                     }
@@ -403,13 +512,13 @@
 
                     if (z > 0) {
                         wtf[people].money = z;
-                        if(x * -1 != 0){
+                        if (x * -1 != 0) {
                             $('#paypaypay').append(`<p style="color:red" >${x*-1} 元<br></p>`);
                         }
                         z = 0;
                     } else {
                         people = people - 1;
-                        if(y != 0){
+                        if (y != 0) {
                             $('#paypaypay').append(`<p style="color:red" >${y} 元<br></p>`);
                         }
                     }
@@ -426,5 +535,122 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
         integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous">
     </script>
+
+
+
+
+    <style>
+        input[type=radio] {
+            zoom: 150%;
+        }
+
+
+        input[type=checkbox] {
+            zoom: 150%;
+        }
+    </style>
+
+
+    <style>
+        body {
+            display: block;
+            margin: 0;
+        }
+
+        div {
+            display: block;
+        }
+
+        label {
+            cursor: default;
+        }
+
+        input {
+            display: inline-block;
+            color: inherit;
+            font: inherit;
+            text-rendering: auto;
+            letter-spacing: normal;
+            word-spacing: normal;
+            text-transform: none;
+            text-indent: 0px;
+            text-shadow: none;
+            text-align: start;
+            -webkit-rtl-ordering: logical;
+            -webkit-writing-mode: horizontal-tb;
+        }
+
+        input[type="checkbox"],
+        input[type="radio"] {
+            -webkit-appearance: none;
+            margin: 0;
+            box-sizing: inherit;
+            border: none;
+            background-color: inherit;
+            padding: 0;
+            cursor: pointer;
+        }
+
+        body {
+            font-family: "Hiragino kaku Gothic Pro", "Meiryo", sans-serif;
+            font-size: 16px;
+            line-height: 1.5;
+        }
+
+        .d1 {
+            margin: 20px;
+            border: 1px dashed black;
+            padding: 20px 0 0 20px;
+            display: flex;
+            flex-wrap: wrap;
+        }
+
+        .d2 {
+            margin: 0px 20px 20px 0px;
+            box-sizing: border-box;
+            max-width: calc(50em + (20px + 1px) * 2);
+            border: 1px solid salmon;
+            background-color: rgba(250, 128, 114, 0.2);
+            padding: 20px 0 0 20px;
+            color: salmon;
+        }
+
+        .d3 {
+            margin: 0 calc(20px - 5px) calc(20px - 5px) 0;
+        }
+
+        .d3 input[type="radio"]+span,
+        .d3 input[type="checkbox"]+span {
+            display: inline-block;
+            margin: 0px 5px 5px 0px;
+            cursor: pointer;
+            border: solid 1px salmon;
+            background-color: rgba(255, 255, 255, 0.9);
+            padding: 10px 20px;
+        }
+
+        .d3 input[type="radio"]:checked+span,
+        .d3 input[type="checkbox"]:checked+span {
+            background-color: rgba(250, 128, 114, 0.7);
+            color: rgba(255, 255, 255, 0.9);
+        }
+
+        .d3 input[type="radio"]:hover+span,
+        .d3 input[type="checkbox"]:hover+span {
+            border-color: rgba(250, 128, 114, 0.5);
+            color: rgba(250, 128, 114, 0.5);
+        }
+
+        .d3 input[type="radio"]:focus+span,
+        .d3 input[type="checkbox"]:focus+span {
+            outline: solid 2px rgba(250, 128, 114, 0.5);
+            outline-offset: 1px;
+        }
+    </style>
+
+
+
+
+
 </body>
  
