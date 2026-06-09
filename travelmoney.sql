5ea5a06676bd8fa907da6b092360eb989826b08d
vincentwongmax <61440757+vincentwongmax@users.noreply.github.com>
2026-06-10 01:35:04 +0800

    Add files via upload

--- travelmoney.sql
+++ travelmoney.sql
@@ -1,16 +1,90 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : 222
+ Source Server         : 127.0.0.1
  Source Server Type    : MySQL
- Source Server Version : 100422
- Source Host           : 192.168.31.219:8867
+ Source Server Version : 100411
+ Source Host           : localhost:3306
  Source Schema         : travelmoney
 
  Target Server Type    : MySQL
- Target Server Version : 100422
+ Target Server Version : 100411
  File Encoding         : 65001
 
- Date: 08/06/2026 17:56:21
+ Date: 30/06/2020 09:05:01
 */
 
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for del
+-- ----------------------------
+DROP TABLE IF EXISTS `del`;
+CREATE TABLE `del`  (
+  `ID` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `paymoneypeople` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `usemoneypeople` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `howmuchmoney` int(255) NULL DEFAULT NULL,
+  `notes` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `adddatatime` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `IDED` int(255) NOT NULL,
+  `IDDD` int(255) NOT NULL AUTO_INCREMENT,
+  `deltime` timestamp(0) NULL DEFAULT current_timestamp(0) ON UPDATE CURRENT_TIMESTAMP(0),
+  PRIMARY KEY (`IDDD`, `IDED`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 63 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of del
+-- ----------------------------
+INSERT INTO `del` VALUES ('123', '你好', 'OK', 13, NULL, NULL, 10, 2, '2020-06-20 17:01:08');
+
+-- ----------------------------
+-- Table structure for mainpeople
+-- ----------------------------
+DROP TABLE IF EXISTS `mainpeople`;
+CREATE TABLE `mainpeople`  (
+  `ID` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `mainpeople` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of mainpeople
+-- ----------------------------
+INSERT INTO `mainpeople` VALUES ('123', 'Vincent');
+
+-- ----------------------------
+-- Table structure for payrecord
+-- ----------------------------
+DROP TABLE IF EXISTS `payrecord`;
+CREATE TABLE `payrecord`  (
+  `ID` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `paymoneypeople` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `usemoneypeople` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `howmuchmoney` double(255, 17) NOT NULL,
+  `notes` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `adddatatime` timestamp(0) NULL DEFAULT current_timestamp(0),
+  `IDED` int(255) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`IDED`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 87 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of payrecord
+-- ----------------------------
+INSERT INTO `payrecord` VALUES ('233', '他', '我,你,他', 6666.00000000000000000, NULL, NULL, 5);
+
+-- ----------------------------
+-- Table structure for token
+-- ----------------------------
+DROP TABLE IF EXISTS `token`;
+CREATE TABLE `token`  (
+  `token` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of token
+-- ----------------------------
+INSERT INTO `token` VALUES ('123', NULL);
+
+SET FOREIGN_KEY_CHECKS = 1;
